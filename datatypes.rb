19d939d48cb0241fdbf16acac6b62122f1e5e104
nimblegeek <62463923+nimblegeek@users.noreply.github.com>
2024-02-06 21:07:05 +0000

    basic datatypes walkthrough

---
+++ datatypes.rb
@@ -0,0 +1,41 @@
+# Numbers
+
+puts 1 + 1
+
+puts 1 - 1
+
+puts 2 * 2
+puts 4 / 2
+puts 2**3
+
+# Addition
+1 + 1 #=> 2
+
+# Subtraction
+2 - 1 #=> 1
+
+# Multiplication
+2 * 2 #=> 4
+
+# Division
+10 / 5 #=> 2
+
+# Exponent
+2**2  #=> 4
+3**4  #=> 81
+
+# Modulus (find the remainder of division)
+8 % 2   #=> 0  (8 / 2 = 4; no remainder)
+10 % 4  #=> 2  (10 / 4 = 2 with a remainder of 2)
+
+# Strings
+
+## Capitalize
+'hello'.capitalize #=> "Hello"
+
+## Upcase
+'hello'.upcase #=> "HELLO"
+
+## Inclue
+'hello'.include?('lo')   #=> true
+'hello'.include?('5')    #=> false
